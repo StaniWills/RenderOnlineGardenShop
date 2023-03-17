122be759a9e92bf300936a070dac16917cb6cd20
StaniWills <deberkader@mail.ru>
2023-03-17 18:36:53 +0100

    update requests

--- src/requests/categories_products_req.js
+++ src/requests/categories_products_req.js
@@ -2,7 +2,7 @@ import { loadCategoryProducts } from "../store/reducers/products_from_category";
 
 export const load_category_products = (id) => {
   return (dispatch) => {
-    fetch(`https://renderonlinegardenshopback.onrender.com/${id}`)
+    fetch(`https://renderonlinegardenshopback.onrender.com/categories/${id}`)
       .then((response) => response.json())
       .then((data) => {
         const payload = data.map((el) => ({
--- src/requests/product_req.js
+++ src/requests/product_req.js
@@ -2,7 +2,7 @@ import { loadProduct } from "../store/reducers/product";
 
 export const load_product = (id) => {
   return (dispatch) => {
-    fetch(`https://renderonlinegardenshopback.onrender.com/${id}`)
+    fetch(`https://renderonlinegardenshopback.onrender.com/products/${id}`)
       .then((resp) => resp.json())
       .then((json) => dispatch(loadProduct(json[0])));
   };
