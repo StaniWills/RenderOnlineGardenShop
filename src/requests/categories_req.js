c8d34ab1c72722553e87ea9d081f2e7b85b866a5
StaniWills <deberkader@mail.ru>
2023-03-17 18:11:07 +0100

    update 1002mal

--- src/requests/categories_req.js
+++ src/requests/categories_req.js
@@ -1,7 +1,7 @@
 import { loadCategories } from "../store/reducers/categories";
 
 export const load_categories = (dispatch) => {
-  fetch("https://telran-project-backend.onrender.com/categories/all")
+  fetch("https://renderonlinegardenshopback.onrender.com/categories/all")
     .then((res) => res.json())
     .then((json) => dispatch(loadCategories(json)));
 };
--- src/requests/product_req.js
+++ src/requests/product_req.js
@@ -2,7 +2,7 @@ import { loadProduct } from "../store/reducers/product";
 
 export const load_product = (id) => {
   return (dispatch) => {
-    fetch(`https://telran-project-backend.onrender.com/${id}`)
+    fetch(`https://renderonlinegardenshopback.onrender.com/${id}`)
       .then((resp) => resp.json())
       .then((json) => dispatch(loadProduct(json[0])));
   };
